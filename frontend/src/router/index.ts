dc848231fd70300f2b0bf482de7d9c408b67b575
Filipe Sales <eduardojs999@gmail.com>
2023-04-15 13:01:33 -0400

    test: document format

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from './routes'
+import { createRouter, createWebHistory } from "vue-router";
+import routes from "./routes";
 
 export default createRouter({
   history: createWebHistory(),
 
-
-
-  
-  routes
-})
+  routes,
+});
