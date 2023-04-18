5cfaa1fb82b391db87dce7f4cf3ee4d121c7eb78
jonas-al <yonas.alluniverse@gmail.com>
2023-04-18 14:45:18 -0300

    fix: nome rotas

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,7 +1,7 @@
 export default [
   { path: '/', component: () => import('../pages/HomePage.vue') },
   { path: '/test', component: () => import('../pages/RouterTest.vue') },
-  { path: '/login', component: () => import('../pages/PaginaLogin.vue') },
-  { path: '/register', component: () => import('../pages/CadastroPerfil.vue') },
-  { path: '/admin-register', component: () => import('../pages/CadastroAdmin.vue') }
+  { path: '/entrar', component: () => import('../pages/PaginaLogin.vue') },
+  { path: '/cadastro-perfil', component: () => import('../pages/CadastroPerfil.vue') },
+  { path: '/cadastro-admin', component: () => import('../pages/CadastroAdmin.vue') }
 ]
