d08dfd78a964654ba697e649f0ceff3f52ea3130
Alfredo Gabriel de Sousa Oliveira <amgabriel1@hotmail.com>
2023-04-19 16:53:34 -0300

    feat: adição de elementos comuns para o tratamento de erro.

---
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/enumeration/ErrorType.java
@@ -0,0 +1,35 @@
+package labes.facomp.ufpa.br.meuegresso.enumeration;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorType {
+
+    REPORT_001("Requisição inválida.", "RP-001"),
+    
+    REPORT_002("Relatório com ID = %s não encontrado.", "RP-002"),
+    
+    REPORT_003("Senha inválida.", "RP-003"),
+    
+    REPORT_004("Recurso não encontrado.", "RP-004"),
+    
+    REPORT_005("Erro no JSON do corpo da requisição.", "RP-005"),
+    
+    REPORT_006("Erro ao enviar o email.", "RP-006"),
+    
+    REPORT_009("Não é permitido enviar lista vazia no corpo da requisição!", "RP-009"),
+
+    DATABASE_001("Falha ao tentar salvar no banco de dados.", "DB-001"),
+    
+    DATABASE_002("Nenhuma mensagem de email do tipo %s cadastrada.", "DB-002"),
+    
+    USER_001("Usuário %s não existe.", "US-001"),
+    
+    private String message;
+    private String internalCode;
+
+    private ErrorType(String message, String internalCode) {
+        this.message = message;
+        this.internalCode = internalCode;
+    }
+}
