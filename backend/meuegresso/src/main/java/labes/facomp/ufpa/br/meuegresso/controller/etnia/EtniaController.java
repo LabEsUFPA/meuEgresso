af6c48c167e0583c694acc18cf3c9a987a32a1b1
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-04-21 19:10:21 -0300

    feat: adição dos dtos

---
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/controller/etnia/EtniaController.java
@@ -0,0 +1,17 @@
+package labes.facomp.ufpa.br.meuegresso.controller.etnia;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import labes.facomp.ufpa.br.meuegresso.service.etnia.EtniaService;
+
+
+@RestController
+@RequestMapping(value = "/etnia")
+public class EtniaController {
+    
+    private EtniaService etniaService;
+
+ 
+    
+}
---
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/dto/etnia/EtniaDTO.java
@@ -0,0 +1,16 @@
+package labes.facomp.ufpa.br.meuegresso.dto.etnia;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EtniaDTO {
+
+    private Integer id;
+
+    private String nome;
+
+}
