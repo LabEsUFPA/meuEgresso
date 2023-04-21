03e72c4fa605cf2695896207c71051cc5d3574e0
jonas-al <yonas.alluniverse@gmail.com>
2023-04-21 18:19:24 -0300

    update: color do InvalidInsert

--- frontend/src/components/InvalidInsert.vue
+++ frontend/src/components/InvalidInsert.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="showAlert"
-    class="flex h-12 gap-x-4 bg-red-600 py-2 px-2 items-center justify-between rounded-md"
+    class="flex h-12 gap-x-4 bg-red-500 py-2 px-2 items-center justify-between rounded-md"
   >
     <SvgIcon
       type="mdi"
