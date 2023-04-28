cca7d4cd05909764068ff5458dfce5ad69406c2d
hawkilol <abacates21@gmail.com>
2023-04-28 11:25:27 -0300

    fix(store): Corrige a url base referente ao .env

--- frontend/src/store/ResponseStore.vue
+++ frontend/src/store/ResponseStore.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
 import { useAuthStore } from 'src/store/AuthStore.vue'
-// import { defineAsyncComponent } from 'vue'
-import Axios from 'axios'
+import axios from 'axios'
 import loginModel from 'src/model/loginModel'
 import egressoModel from 'src/model/egressoModel'
-const baseUrl = `${import.meta.env.VITE_API_URL}`
-const loginUrl = `'${baseUrl}/auth/login'`
-// login
+
+const Axios = axios.create({
+  baseURL: import.meta.env.VITE_API_URL_LOCAL
+})
 
 export async function sendLogin (username: string, password: string) {
   const data: loginModel = {
@@ -15,22 +15,9 @@ export async function sendLogin (username: string, password: string) {
   }
   const response = await Axios({
     method: 'post',
-    url: 'http://localhost:15000/auth/login',
+    url: '/auth/login',
     data
   })
-  // .then(response => {
-  //     console.log(response.data)
-  //     if (response.status === 201) {
-  //       submitSuccess.value = true
-  //     }
-  //   })
-  //   .catch(response => {
-  //     console.log(response.data)
-  //     if (response.response.status === 401) {
-  //       errorText.value = errorMessages.value.errorRequest
-  //       error.value = true
-  //     }
-  //   })
   console.log(baseUrl)
   const token = response.data.token
 
@@ -44,7 +31,7 @@ export async function sendLoginModel (data: loginModel) {
   console.log(baseUrl)
   const response = await Axios({
     method: 'post',
-    url: 'http://localhost:15000/auth/login',
+    url: '/auth/login',
     data
   })
 
@@ -64,7 +51,7 @@ export async function updateEgressoDataModel (data: egressoModel) {
   console.log(data)
   const response = await Axios({
     method: 'put',
-    url: 'http://localhost:15000/egresso',
+    url: '/egresso',
     data,
     headers: {
       Authorization: `Bearer ${token}`
@@ -79,7 +66,7 @@ export async function postEgressoDataModel (data: egressoModel) {
   console.log(data)
   const response = await Axios({
     method: 'post',
-    url: 'http://localhost:15000/egresso',
+    url: '/egresso',
     data,
     headers: {
       Authorization: `Bearer ${token}`
@@ -88,38 +75,5 @@ export async function postEgressoDataModel (data: egressoModel) {
   console.log(response)
   return response
 }
-
-export async function sendEgressoData () {
-  // const token = useAuthStore().getUserToken()
-  // const data: egressoModel = {
-  //   // String to number >:)
-
-  //   matricula: +dataEgresso.value.academico.matricula,
-  //   genero: {
-  //     nome: dataEgresso.value.geral.genero
-  //   },
-  //   cotista: dataEgresso.value.academico.cotista.value,
-  //   pcd: dataEgresso.value.academico.bolsista.value,
-  //   interesseEmPos: dataEgresso.value.academico.posGrad.value,
-  //   lattes: dataEgresso.value.geral.lattes,
-  //   linkedin: dataEgresso.value.geral.linkedin,
-  //   endereco: {
-  //     cidade: dataEgresso.value.localizacao.cidade,
-  //     estado: dataEgresso.value.localizacao.estado,
-  //     pais: dataEgresso.value.localizacao.pais
-  //   }
-  // }
-  // const response = await Axios({
-  //   method: 'post',
-  //   url: '${baseUrl}/egresso',
-  //   data,
-  //   headers: {
-  //     Authorization: `Bearer ${token}`
-  //   }
-  // })
-  //  return response
-
-}
-//
 
 </script>
