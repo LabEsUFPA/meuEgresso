4031d6811f3aaae1b870eb21a2c201512d7e0433
Pedro Inacio <pedroinacio1939@gmail.com>
2023-04-29 03:27:21 -0300

    test: adicionado mais testes do egresso

---
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/repository/egresso/EgressoEmpresaRepositoryTest.java
@@ -0,0 +1,88 @@
+package labes.facomp.ufpa.br.meuegresso.repository.egresso;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import labes.facomp.ufpa.br.meuegresso.model.EgressoEmpresaModel;
+import labes.facomp.ufpa.br.meuegresso.model.EgressoEmpresaModelId;
+import labes.facomp.ufpa.br.meuegresso.model.EgressoModel;
+import labes.facomp.ufpa.br.meuegresso.model.EmpresaModel;
+import labes.facomp.ufpa.br.meuegresso.model.FaixaSalarialModel;
+
+/**
+ * Classe que implementa testes das funcionalides de EgressoEmpresaRepository
+ *
+ * @author Pedro Inácio
+ * @since 28/04/2023
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+
+public class EgressoEmpresaRepositoryTest {
+
+    @Autowired
+    private EgressoEmpresaRepository egressoEmpresaRepository;
+
+    private EgressoEmpresaModel testarEgressoEmpresa;
+
+    @BeforeEach
+    public void setUp() {
+        testarEgressoEmpresa = new EgressoEmpresaModel();
+        testarEgressoEmpresa.setId(new EgressoEmpresaModelId());
+        testarEgressoEmpresa.setAreaAtuacao("area");
+        testarEgressoEmpresa.setEgresso(new EgressoModel());
+        testarEgressoEmpresa.setEmpresa(new EmpresaModel());
+        testarEgressoEmpresa.setFaixaSalarial(new FaixaSalarialModel());
+
+        egressoEmpresaRepository.save(testarEgressoEmpresa);
+    }
+
+    // @Test
+    /*
+     * public void testSave() {
+     *
+     * EgressoEmpresaModel.builder()
+     * .id(1)
+     * .nascimento(null)
+     * .interesseEmPos(true)
+     * .lattes("https://lattes.cnpq.br/")
+     * .linkedin("https://linkedin.com/")
+     * .build();
+     *
+     * EgressoEmpresaModel response = egressoEmpresaRepository.save(testarEgressoEmpresa);
+     * assertNotNull(response);
+     * }
+     */
+
+    @Test
+    public void testFindAll() {
+
+        List<EgressoEmpresaModel> testar = egressoEmpresaRepository.findAll();
+
+        assertNotNull(testar);
+    }
+    
+    @Test
+    public void testExistsByIdAndCreatedById() {
+
+        Boolean response = egressoEmpresaRepository.existsByIdAndCreatedById(
+                testarEgressoEmpresa.getId(), testarEgressoEmpresa.getCreatedBy().getId());
+
+        assertTrue(response);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testarEgressoEmpresa = null;
+    }
+
+}
--- backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/repository/egresso/EgressoRepositoryTest.java
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/repository/egresso/EgressoRepositoryTest.java
@@ -3,6 +3,7 @@ package labes.facomp.ufpa.br.meuegresso.repository.egresso;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -12,22 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import labes.facomp.ufpa.br.meuegresso.model.CotaModel;
 import labes.facomp.ufpa.br.meuegresso.model.EgressoModel;
+import labes.facomp.ufpa.br.meuegresso.model.EnderecoModel;
+import labes.facomp.ufpa.br.meuegresso.model.GeneroModel;
 
 /**
- * Class that implements tests of the UserAccountRepository functionalities
+ * Classe que implementa testes das funcionalides de EgressoRepository
  *
- * @author Mariana Azevedo
- * @since 06/12/2020
+ * @author Pedro Inácio
+ * @since 28/04/2023
  */
 @SpringBootTest
-// @AutoConfigureMockMvc
 @ActiveProfiles("test")
-/*
- * @TestInstance(Lifecycle.PER_CLASS)
- *
- * @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
- */
 public class EgressoRepositoryTest {
 
     @Autowired
@@ -39,60 +37,34 @@ public class EgressoRepositoryTest {
     public void setUp() {
         testarEgresso = new EgressoModel();
         testarEgresso.setId(1);
-        testarEgresso.setNascimento(null);
+        testarEgresso.setNascimento(LocalDate.parse("1999-10-20"));
         testarEgresso.setInteresseEmPos(true);
         testarEgresso.setLattes("https://lattes.cnpq.br/");
         testarEgresso.setLinkedin("https://linkedin.com/");
-        /*
-         * testarEgresso.setGenero(null);
-         * testarEgresso.setEndereco(null);
-         * testarEgresso.setUsuario(null);
-         * testarEgresso.setDepoimentos(null);
-         */
+        testarEgresso.setCota(new CotaModel(1, "null", null));
+        testarEgresso.setMatricula("20200464222");
+        testarEgresso.setEndereco(new EnderecoModel(1, "null", "null", "null", null));
+        testarEgresso.setGenero(new GeneroModel(1, "null"));
 
         egressoRepository.save(testarEgresso);
-        /*
-         * testarEgresso = EgressoModel.builder()
-         * .id(1)
-         * .nascimento(null)
-         * .interesseEmPos(true)
-         * .lattes("https://lattes.cnpq.br/")
-         * .linkedin("https://linkedin.com/")
-         * .build();
-         */
-
-        /*
-         * EgressoModel response = egressoRepository.save(testarEgresso);
-         * assertNotNull(response);
-         */
     }
 
-    /**
-     * Method that test the repository that save an object Travel in the API.
-     *
-     * @author
-     * @since 24/03/2020
-     */
-    // @Test
-    // @Order(1)
-    /*
-     * public void testSave() {
-     *
-     * EgressoModel.builder()
-     * .id(1)
-     * .nascimento(null)
-     * .interesseEmPos(true)
-     * .lattes("https://lattes.cnpq.br/")
-     * .linkedin("https://linkedin.com/")
-     * .build();
-     *
-     * EgressoModel response = egressoRepository.save(testarEgresso);
-     * assertNotNull(response);
-     * }
-     */
+    @Test
+    public void testSave() {
+    
+     EgressoModel.builder()
+        .id(1)
+        .nascimento(LocalDate.parse("1999-10-20"))
+        .interesseEmPos(true)
+        .lattes("https://lattes.cnpq.br/")
+        .linkedin("https://linkedin.com/")
+        .build();
+        EgressoModel response = egressoRepository.save(testarEgresso);
+        assertNotNull(response);
+    }
+
 
     @Test
-    // @Order(2)
     public void testFindAll() {
 
         List<EgressoModel> testar = egressoRepository.findAll();
@@ -101,14 +73,12 @@ public class EgressoRepositoryTest {
     }
 
     @Test
-    // @Order(3)
     public void testfindByUsuarioId(Integer idUsuario) {
         EgressoModel testar = egressoRepository.findByUsuarioId(idUsuario).orElseThrow();
         assertNotNull(testar);
     }
 
     @Test
-    // @Order(4)
     public void testExistsByIdAndCreatedById() {
 
         Boolean response = egressoRepository.existsByIdAndCreatedById(
