35bf0bd78cb1fdec20e76b5d7a4cd6de6240b513
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-05-01 13:56:24 -0300

    feat: vai

--- backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/dto/egresso/EgressoCadastroDTO.java
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/dto/egresso/EgressoCadastroDTO.java
@@ -3,7 +3,6 @@ package labes.facomp.ufpa.br.meuegresso.dto.egresso;
 import java.time.LocalDate;
 import java.util.Set;
 
-import jakarta.persistence.Convert;
 import labes.facomp.ufpa.br.meuegresso.dto.contribuicao.ContribuicaoDTO;
 import labes.facomp.ufpa.br.meuegresso.dto.cota.CotaDTO;
 import labes.facomp.ufpa.br.meuegresso.dto.depoimento.DepoimentoDTO;
@@ -11,7 +10,6 @@ import labes.facomp.ufpa.br.meuegresso.dto.empresa.EmpresaDTO;
 import labes.facomp.ufpa.br.meuegresso.dto.palestras.PalestrasDTO;
 import labes.facomp.ufpa.br.meuegresso.dto.titulacao.TitulacaoEgressoDTO;
 import labes.facomp.ufpa.br.meuegresso.dto.usuario.UsuarioDTO;
-import labes.facomp.ufpa.br.meuegresso.util.ParseString;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,10 +35,8 @@ public class EgressoCadastroDTO {
 
     private Boolean interesseEmPos = false;
 
-    @Convert(converter = ParseString.class)
     private String lattes;
 
-    @Convert(converter = ParseString.class)
     private String linkedin;
 
     private Boolean posGraduacao = false;
