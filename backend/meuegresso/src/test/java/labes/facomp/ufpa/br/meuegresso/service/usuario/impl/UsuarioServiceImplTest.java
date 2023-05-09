215deafd9d6ca2a47d4109313a477c1cdcffdea5
Tsumioshino <marcus.oliveira@icen.ufpa.br>
2023-05-09 15:31:57 -0300

    fix: refazendo meus testes unitários lixos

--- backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/service/usuario/impl/UsuarioServiceImplTest.java
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/service/usuario/impl/UsuarioServiceImplTest.java
@@ -2,12 +2,19 @@ package labes.facomp.ufpa.br.meuegresso.service.usuario.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -71,118 +78,186 @@ class UsuarioServiceImplTest {
 		usuario2.setPassword("password124");
 		usuario2.setCreatedBy(usuario);
 		usuarioRepository.save(usuario2);
+
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+
 	}
 
 	@Test
 	void test_Given_Valid_Username_Should_Return_Userdata() {
+		Mockito.when(usuarioRepository.findByUsernameIgnoreCase(anyString())).thenReturn(Optional.of(usuario));
 
-		Mockito.when(usuarioRepository.findByUsernameIgnoreCase("john123")).thenReturn(Optional.of(usuario));
+		UsuarioModel user = usuarioService.loadUserByUsername("john123");
 
-		assertEquals(usuario, usuarioService.loadUserByUsername("john123"));
+		assertNotNull(user);
+		assertEquals(usuario, user);
+		Mockito.verify(usuarioRepository).findByUsernameIgnoreCase(anyString());
 	}
 
 	@Test
 	void test_Given_Invalid_Username_Should_Not_Return_Userdata() {
-		Mockito.when(usuarioRepository.findByUsernameIgnoreCase("carla123")).thenReturn(Optional.empty());
+		Mockito.when(usuarioRepository.findByUsernameIgnoreCase(anyString())).thenReturn(Optional.empty());
 
 		assertThrows(UsernameNotFoundException.class, () -> usuarioService.loadUserByUsername("carla123"));
+		Mockito.verify(usuarioRepository).findByUsernameIgnoreCase(anyString());
 	}
 
 	@Test
 	void test_Given_Valid_Username_Should_Return_True() {
-		Boolean username_in_database = true;
-		Mockito.when(usuarioRepository.existsByUsername("john123")).thenReturn(username_in_database);
+		Mockito.when(usuarioRepository.existsByUsername(anyString())).thenReturn(true);
 
-		assertEquals(username_in_database, usuarioService.existsByUsername("john123"));
+		boolean userindb = usuarioService.existsByUsername("john123");
+
+		assertTrue(userindb);
+		Mockito.verify(usuarioRepository).existsByUsername(anyString());
+
 	}
 
 	@Test
 	void test_Given_Invalid_Username_Should_False() {
-		Boolean username_in_database = true;
-		Mockito.when(usuarioRepository.existsByUsername("carla123")).thenReturn(username_in_database);
+		Mockito.when(usuarioRepository.existsByUsername(anyString())).thenReturn(false);
 
-		assertEquals(username_in_database, usuarioService.existsByUsername("carla123"));
+		boolean userindb = usuarioService.existsByUsername("carla123");
+
+		assertFalse(userindb);
+		Mockito.verify(usuarioRepository).existsByUsername(anyString());
 	}
 
 	@Test
 	void test_Given_Valid_Id_Should_Return_Userdata() {
-		Mockito.when(usuarioRepository.findById(1)).thenReturn(Optional.of(usuario));
-		assertEquals(usuario, usuarioService.findById(1));
+		Mockito.when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(usuario));
+
+		UsuarioModel user = usuarioService.findById(1);
+
+		// Idealmente deveria ser algo como anyInt, mas anyInt retorna 0. E ID > 0
+		MatcherAssert.assertThat(user.getId(), Matchers.greaterThan(0));
+		assertNotNull(user);
+		assertEquals(usuario, user);
+		Mockito.verify(usuarioRepository).findById(anyInt());
 	}
 
 	@Test
 	void test_Given_Invalid_Id_Should_Not_Return_Userdata() {
-		Mockito.when(usuarioRepository.findById(10)).thenReturn(Optional.of(usuario));
+		Mockito.when(usuarioRepository.findById(anyInt())).thenReturn(Optional.empty());
+
 		assertThrows(NoSuchElementException.class, () -> usuarioService.findById(1));
+		Mockito.verify(usuarioRepository).findById(anyInt());
 	}
 
 	@Test
 	void test_Should_Return_All_Userdata() {
-		List<UsuarioModel> users = new ArrayList<>();
 		Mockito.when(usuarioRepository.findAll()).thenReturn(usuarios);
-		users = usuarioService.findAll();
+
+		List<UsuarioModel> users = usuarioService.findAll();
+
+		assertNotNull(users);
+		assertEquals(usuarios.size(), users.size());
 		assertEquals(usuarios, users);
+		Mockito.verify(usuarioRepository).findAll();
 	}
 
 	@Test
 	void test_Save_Should_Save_On_Repository() {
-		Mockito.when(usuarioRepository.save(usuario)).thenReturn(usuario);
-		UsuarioModel usuariotest = usuarioService.save(usuario);
-		assertEquals(usuario, usuariotest);
+		Mockito.when(usuarioRepository.save(new UsuarioModel())).thenReturn(usuario);
+		UsuarioModel user = usuarioService.save(new UsuarioModel());
+		assertNotNull(user);
+		assertEquals(usuario, user);
+		Mockito.verify(usuarioRepository).save(new UsuarioModel());
+
 	}
 
 	@Test
 	void test_Given_New_Credentials_Return_Updated_User() throws InvalidRequestException {
-		usuario.setNome("Michael");
-		usuario.setEmail("michel@hotmail.com");
-		usuario.setUsername("michel123");
-		usuario.setLastModifiedBy(usuario);
+		// something like clone, but already in db
+		UsuarioModel user = new UsuarioModel();
+		user.setId(1);
+		user.setNome("John");
+		user.setUsername("john123");
+		user.setEmail("john@example.com");
+		user.setPassword("password123");
+		user.setCreatedBy(usuario);
+		// updating
+		user.setNome("Michael");
+		user.setEmail("michel@hotmail.com");
+		user.setUsername("michel123");
+		user.setPassword("newpassword");
+		user.setLastModifiedBy(user);
 
-		Mockito.when(usuarioRepository.save(usuario)).thenReturn(usuario);
+		UsuarioModel mockUser = new UsuarioModel();
+		mockUser.setId(10);
 
-		UsuarioModel usuario2 = new UsuarioModel();
-		usuario2.setId(1);
-		usuario2.setNome("John");
-		usuario2.setUsername("john123");
-		usuario2.setEmail("john@example.com");
-		usuario2.setPassword("password123");
-		usuario2.setCreatedBy(usuario2);
+		Mockito.when(usuarioRepository.save(new UsuarioModel(10, null, null, null, null, null, null)))
+				.thenReturn(user);
+		UsuarioModel usertest = usuarioService.update(mockUser);
 
-		usuario2.setNome("Michael");
-		usuario2.setEmail("michel@hotmail.com");
-		usuario2.setUsername("michel123");
-		usuario2.setLastModifiedBy(usuario2);
+		MatcherAssert.assertThat(usertest.getId(), Matchers.greaterThan(0));
+		assertNotNull(usertest);
+		assertEquals(user, usertest);
+		Mockito.verify(usuarioRepository).save(new UsuarioModel(10, null, null, null, null, null, null));
+	}
 
-		assertEquals(usuario, usuarioService.update(usuario2));
+	@Test
+	void test_Given_New_Credentials_Do_Not_Return_Updated_For_Wrong_User() throws InvalidRequestException {
+		// something like clone, but already in db
+		UsuarioModel user = new UsuarioModel();
+		user.setId(10);
+		user.setNome("John");
+		user.setUsername("john123");
+		user.setEmail("john@example.com");
+		user.setPassword("password123");
+		user.setCreatedBy(usuario);
+		// updating
+		user.setNome("Michael");
+		user.setEmail("michel@hotmail.com");
+		user.setUsername("michel123");
+		user.setPassword("newpassword");
+		user.setLastModifiedBy(user);
+
+		UsuarioModel mockUser = new UsuarioModel();
+
+		assertThrows(InvalidRequestException.class, () -> usuarioService.update(mockUser));
 	}
 
 	@Test
 	void test_Given_Correct_Id_Should_Delete_User_Data() {
-		Mockito.when(usuarioService.deleteById(1)).thenReturn(true);
-		assertThrows(NoSuchElementException.class, () -> usuarioService.findById(1));
+		Mockito.when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(usuario));
+		Mockito.when(usuarioRepository.existsById(1)).thenReturn(true);
+
+		UsuarioModel user = usuarioService.findById(1);
+
+		assertTrue(usuarioService.deleteById(user.getId()));
+		Mockito.verify(usuarioRepository).deleteById(user.getId());
 	}
 
 	@Test
 	void test_Given_Incorrect_Id_Should_Not_Delete_User_Data() {
-		assertFalse(usuarioService.deleteById(10));
+		Mockito.when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(usuario));
+		Mockito.when(usuarioRepository.existsById(1)).thenReturn(false);
+
+		UsuarioModel user = usuarioService.findById(1);
+
+		assertFalse(usuarioService.deleteById(user.getId()));
+		Mockito.verify(usuarioRepository, Mockito.times(0)).deleteById(user.getId());
 	}
 
 	@Test
 	void test_Given_Id_And_IdFromTheOneThatCreatedTheId_Return_True_If_Right() {
-		Boolean right = true;
-		Mockito.when(usuarioRepository.existsByIdAndCreatedById(2, 1)).thenReturn(right);
-		assertEquals(right, usuarioService.existsByIdAndCreatedById(2, 1));
+		Mockito.when(usuarioRepository.existsByIdAndCreatedById(anyInt(), anyInt())).thenReturn(true);
+		assertTrue(usuarioService.existsByIdAndCreatedById(1, 2));
+		Mockito.verify(usuarioRepository).existsByIdAndCreatedById(anyInt(), anyInt());
 	}
 
 	@Test
 	void test_Given_Id_And_IdFromTheOneThatCreatedTheId_Return_False_If_Wrong() {
-		Boolean right = false;
-		Mockito.when(usuarioRepository.existsByIdAndCreatedById(1, 2)).thenReturn(right);
-		assertEquals(right, usuarioService.existsByIdAndCreatedById(1, 2));
+		Mockito.when(usuarioRepository.existsByIdAndCreatedById(anyInt(), anyInt())).thenReturn(false);
+		assertFalse(usuarioService.existsByIdAndCreatedById(5, 5));
+		Mockito.verify(usuarioRepository).existsByIdAndCreatedById(anyInt(), anyInt());
 	}
 
 	@AfterEach
 	void tearDown() {
 		usuario = null;
+		usuarios = null;
 	}
 }
