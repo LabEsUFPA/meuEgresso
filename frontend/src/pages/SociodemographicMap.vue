80f3ea54f9758fe7ea66ab5bb79240c72576abc0
jonas-al <yonas.alluniverse@gmail.com>
2023-05-09 22:29:28 -0300

    feat: inicializando o mapa com a lib leaflet

---
+++ frontend/src/pages/SociodemographicMap.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="height:600px; width:800px">
+    <l-map
+      ref="map"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      :use-global-leaflet="false"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      />
+      <l-circle-marker
+        @click="testClick"
+        :lat-lng="test"
+      />
+    </l-map>
+  </div>
+</template>
+
+<script setup lang="ts">
+import 'leaflet/dist/leaflet.css'
+import { latLng } from 'leaflet'
+import { LMap, LTileLayer, LCircleMarker } from '@vue-leaflet/vue-leaflet'
+
+const zoom = 2
+const test = latLng(-1.45502, -48.5024)
+const testClick = (ClickEvent: Event) => {
+  console.log(ClickEvent)
+}
+</script>
