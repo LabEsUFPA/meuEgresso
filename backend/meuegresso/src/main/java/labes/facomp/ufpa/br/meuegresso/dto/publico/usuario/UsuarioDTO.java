1407fc54848b37e777921ac4efd6a423c898072c
Tsumioshino <marq938@gmail.com>
2023-05-14 02:08:03 -0300

    feat: dto publico pra egresso e usuario

---
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/dto/publico/usuario/UsuarioDTO.java
@@ -0,0 +1,25 @@
+package labes.facomp.ufpa.br.meuegresso.dto.publico.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Encapsulamento da tabela Usuario a fim de representar os dados publicos representados pelo Egresso
+ *
+ * @author Marcus Maciel Oliveira
+ * @since 13/05/2023
+ * @version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class UsuarioDTO  {
+
+	private String email;
+
+	private String nome;
+	
+}
