1809d3d27fec5dd161c2d9bcf8f2f5705be7d6e3
chsantos234 <camilosantos1709@gmail.com>
2023-05-13 21:37:50 -0300

    test: desenvolvimento unit test foto do egresso

---
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/controller/egresso/EgressoControllerTest.java
@@ -0,0 +1,197 @@
+package labes.facomp.ufpa.br.meuegresso.controller.egresso;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.util.ResourceUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import labes.facomp.ufpa.br.meuegresso.dto.auth.AuthenticationRequest;
+import labes.facomp.ufpa.br.meuegresso.dto.auth.AuthenticationResponse;
+import labes.facomp.ufpa.br.meuegresso.dto.contribuicao.ContribuicaoDTO;
+import labes.facomp.ufpa.br.meuegresso.dto.depoimento.DepoimentoDTO;
+import labes.facomp.ufpa.br.meuegresso.dto.egresso.EgressoCadastroDTO;
+import labes.facomp.ufpa.br.meuegresso.dto.egresso.EgressoPublicDTO;
+import labes.facomp.ufpa.br.meuegresso.enumeration.ResponseType;
+import labes.facomp.ufpa.br.meuegresso.model.EgressoModel;
+import labes.facomp.ufpa.br.meuegresso.model.GeneroModel;
+import labes.facomp.ufpa.br.meuegresso.model.GrupoModel;
+import labes.facomp.ufpa.br.meuegresso.model.TitulacaoModel;
+import labes.facomp.ufpa.br.meuegresso.model.UsuarioModel;
+import labes.facomp.ufpa.br.meuegresso.repository.genero.GeneroRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.grupo.GrupoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.titulacao.TitulacaoRepository;
+
+@SpringBootTest
+@DirtiesContext
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(OrderAnnotation.class)
+public class EgressoControllerTest {
+
+    @Autowired
+    private TitulacaoRepository titulacaoRepository;
+
+    @Autowired
+    private GrupoRepository grupoRepository;
+
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    String token;
+
+    EgressoModel egressoModel;
+
+    UsuarioModel usuarioModel;
+
+    GeneroModel genero;
+
+    ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+
+    EgressoPublicDTO egressoPublicDTO;
+
+    EgressoCadastroDTO egressoCadastro = new EgressoCadastroDTO();
+
+    static final Integer EGRESSO_ID = 1;
+    static final String EGRESSO_EMAIL = "teste@gmail.com";
+
+    @BeforeAll
+    void setUp() throws Exception {
+        
+        titulacaoRepository.save(TitulacaoModel.builder().nome("abc").build());
+        titulacaoRepository.save(TitulacaoModel.builder().nome("123").build());
+
+        genero = new GeneroModel(43, "genero X");
+        genero = generoRepository.save(genero);
+
+        GrupoModel grupoModel = new GrupoModel();
+        grupoModel.setNomeGrupo("ADMIN");
+
+        grupoModel = grupoRepository.save(grupoModel);
+
+        Set<GrupoModel> grupos = new HashSet<>();
+        grupos.add(grupoModel);
+
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setUsername("username");
+        usuarioModel.setNome("nome_test");
+        usuarioModel.setEmail("teste@gmail.com");
+        usuarioModel.setPassword("teste123");
+        usuarioModel.setGrupos(grupos);
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(usuarioModel)))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(status().isCreated())
+                        .andReturn();
+
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.setUsername(usuarioModel.getUsername());
+        authenticationRequest.setPassword(usuarioModel.getPassword());
+        String objectJson = objectMapper.writeValueAsString(authenticationRequest);
+
+        MvcResult resultado = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectJson))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(status().isOk())
+                        .andReturn();
+        AuthenticationResponse authenticationResponse = objectMapper.readValue(
+                        resultado.getResponse().getContentAsString(), AuthenticationResponse.class);
+        this.token = authenticationResponse.getToken();
+
+        egressoCadastro.setNascimento(LocalDate.now());
+        egressoCadastro.setGeneroId(EGRESSO_ID);
+        egressoCadastro.setMatricula("123432312345");
+        egressoCadastro.setNome("teste_nome");
+        egressoCadastro.setContribuicao(ContribuicaoDTO.builder().descricao("ljhfdakljdljdhs").build());
+        egressoCadastro.setDepoimento(DepoimentoDTO.builder().descricao("sffsfsffd").build());
+
+    }
+
+    @Test
+    @Order(1)
+    void testCadastrarEgressoPrimeiroCadastro() throws Exception {
+
+
+            MvcResult resposta = mockMvc.perform(
+                            MockMvcRequestBuilders.post("/egresso")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .content(objectMapper.writeValueAsString(egressoCadastro))
+                                            .header("Authorization", "Bearer " + this.token))
+                            .andDo(MockMvcResultHandlers.print())
+                            .andExpect(status().isCreated()).andReturn();
+
+            String resp = resposta.getResponse().getContentAsString();
+            assertEquals(ResponseType.SUCESS_SAVE.getMessage(), resp);
+    }
+
+    @Test
+    @Order(2)
+    void testSaveFotoEgresso() throws IOException, Exception {
+        
+        Path path = ResourceUtils.getFile("classpath:image/imagem.jpeg").toPath();
+
+        MockMultipartFile file = new MockMultipartFile("arquivo", "imagem.jpeg", "image/jpeg",Files.readAllBytes(path));
+        
+        MvcResult resposta = mockMvc.perform(
+                MockMvcRequestBuilders.multipart("/egresso/foto")
+                        .file(file)
+            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+            .header("Authorization", "Bearer " + this.token))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isCreated()).andReturn();
+        
+        String resultado = resposta.getResponse().getContentAsString();
+
+        assertEquals(resultado,ResponseType.SUCESS_IMAGE_SAVE.getMessage());
+
+    }
+
+    @Test
+    @Order(3)
+    void testGetFotoEgresso() {
+        
+    }
+
+    @Test
+    @Order(4)
+    void testDeleteFotoEgresso() {
+    }
+}
