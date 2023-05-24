c99f97d364d81a251a6d8d0a2ea420626f9108b0
jonas-al <yonas.alluniverse@gmail.com>
2023-05-24 14:44:16 -0300

    test: iniciando o teste do componente CustomBarGraph

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import paths from 'vite-tsconfig-paths'
@@ -7,10 +8,18 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue(), paths()],
   resolve: {
+    mainFields: ["module"],
     alias: {
       src: path.resolve(__dirname, 'src/'),
       components: path.resolve(__dirname, 'src/components/'),
       pages: path.resolve(__dirname, 'src/pages/')
+    }
+  },
+  test: {
+    deps: {
+      inline: [
+        "echarts"
+      ]
     }
   }
 })
