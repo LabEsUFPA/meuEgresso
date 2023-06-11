1a9c3869674aafa699e69922db2097d01235522b
Pedro Inacio <pedroinacio1939@gmail.com>
2023-06-11 00:09:49 -0300

    test: adicionado Endereco e SetorAtuacao RepositoryTest

---
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/repository/endereco/EnderecoRepositoryTest.java
@@ -0,0 +1,169 @@
+package labes.facomp.ufpa.br.meuegresso.repository.endereco;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import labes.facomp.ufpa.br.meuegresso.model.EgressoModel;
+import labes.facomp.ufpa.br.meuegresso.model.GeneroModel;
+import labes.facomp.ufpa.br.meuegresso.model.GrupoModel;
+import labes.facomp.ufpa.br.meuegresso.model.EnderecoModel;
+import labes.facomp.ufpa.br.meuegresso.model.UsuarioModel;
+import labes.facomp.ufpa.br.meuegresso.repository.egresso.EgressoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.genero.GeneroRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.grupo.GrupoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.usuario.UsuarioRepository;
+
+/**
+ * Classe que testa as features do EnderecoRepository
+ *
+ * @author Pedro Inácio
+ * @since 10/06/2023
+ */
+@DirtiesContext
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
+class EnderecoRepositoryTest {
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    
+    @Autowired
+    private EgressoRepository egressoRepository;
+
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private GrupoRepository grupoRepository;
+
+    private EnderecoModel enderecoModel;
+        
+    private GrupoModel grupoModel;
+    
+    private EgressoModel egressoModel;
+    
+    private GeneroModel generoModel;
+
+    private UsuarioModel usuarioModel;
+
+    private final static Integer ID = 1;
+    private final static String CIDADE = "cidadeTeste";
+    private final static String ESTADO = "estadoTeste";
+    private final static String PAIS = "paisTeste";
+
+
+    @BeforeEach
+    void setUp() {
+        generoModel = new GeneroModel();
+        generoModel.setId(1);
+        generoModel.setNome("Masculino");
+        generoModel = generoRepository.save(generoModel);
+
+        grupoModel = GrupoModel.builder().id(1).nomeGrupo("ADMIN").build();
+        grupoModel = grupoRepository.save(grupoModel);
+        Set<GrupoModel> grupos = new HashSet<>();
+        grupos.add(grupoModel);
+
+        usuarioModel = UsuarioModel.builder()
+                .id(1)
+                .nome("John")
+                .username("john123")
+                .email("john@example.com")
+                .password("password123")
+                .grupos(grupos)
+                .build();
+        usuarioModel = usuarioRepository.save(usuarioModel);
+
+        egressoModel = EgressoModel.builder()
+                .id(1)
+                .nascimento(LocalDate.parse("1999-10-20"))
+                .genero(generoModel)
+                .matricula("202003940011")
+                .interesseEmPos(true)
+                .lattes("null")
+                .linkedin("null")
+                .usuario(usuarioModel)
+                .build();
+        egressoModel.setCreatedBy(usuarioModel);
+
+        egressoModel = egressoRepository.save(egressoModel);
+
+        enderecoModel = EnderecoModel.builder()
+            .id(ID)
+            .cidade(CIDADE)
+            .estado(ESTADO)
+            .pais(PAIS)
+            .build();
+
+        enderecoModel.setCreatedBy(usuarioModel);
+
+        enderecoModel = enderecoRepository.save(enderecoModel);
+    }
+
+    /**
+     * Método que testa o repositório que retorna todos os Enderecos
+     *
+     * @author Pedro Inácio
+     * @since 10/06/2023
+     */
+    @Test
+    void testFindAll() {
+        List<EnderecoModel> response = enderecoRepository.findAll();
+
+        assertNotNull(response);
+        assertEquals(List.of(enderecoModel), response);
+    }
+    
+    @Test
+    void testFindByCidadeIgnoreCaseAndEstadoIgnoreCaseAndPaisIgnoreCase() {
+        Optional<EnderecoModel> response = enderecoRepository.findByCidadeIgnoreCaseAndEstadoIgnoreCaseAndPaisIgnoreCase(enderecoModel.getCidade(), enderecoModel.getEstado(), enderecoModel.getPais());
+        
+        assertNotNull(response);
+        assertEquals(enderecoModel, response.get());
+    }
+    
+    @Test
+    void testExistsByIdAndCreatedById() {
+        Boolean response = enderecoRepository.existsByIdAndCreatedById(enderecoModel.getId(), enderecoModel.getCreatedBy().getId());
+
+        assertNotNull(response);
+        assertTrue(response);
+    }
+
+    /**
+     * Metodo para remover todos os dados do repository.
+     *
+     * @author Pedro Inácio
+     * @since 10/06/2023
+     */
+    @AfterAll
+    public void tearDown() {
+        enderecoRepository.deleteAll();
+    }
+}
---
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/repository/setoratuacao/SetorAtuacaoRepositoryTest.java
@@ -0,0 +1,164 @@
+package labes.facomp.ufpa.br.meuegresso.repository.setoratuacao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import labes.facomp.ufpa.br.meuegresso.model.EgressoModel;
+import labes.facomp.ufpa.br.meuegresso.model.GeneroModel;
+import labes.facomp.ufpa.br.meuegresso.model.GrupoModel;
+import labes.facomp.ufpa.br.meuegresso.model.SetorAtuacaoModel;
+import labes.facomp.ufpa.br.meuegresso.model.UsuarioModel;
+import labes.facomp.ufpa.br.meuegresso.repository.egresso.EgressoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.genero.GeneroRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.grupo.GrupoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.usuario.UsuarioRepository;
+
+/**
+ * Classe que testa as features do SetorAtuacaoRepository
+ *
+ * @author Pedro Inácio
+ * @since 10/06/2023
+ */
+@DirtiesContext
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
+class SetorAtuacaoRepositoryTest {
+
+    @Autowired
+    private SetorAtuacaoRepository setorAtuacaoRepository;
+    
+    @Autowired
+    private EgressoRepository egressoRepository;
+
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private GrupoRepository grupoRepository;
+
+    private SetorAtuacaoModel setorAtuacaoModel;
+        
+    private GrupoModel grupoModel;
+    
+    private EgressoModel egressoModel;
+    
+    private GeneroModel generoModel;
+
+    private UsuarioModel usuarioModel;
+
+    private final static Integer ID = 1;
+    private final static String NOME = "nomeTeste";
+
+    @BeforeEach
+    void setUp() {
+        generoModel = new GeneroModel();
+        generoModel.setId(1);
+        generoModel.setNome("Masculino");
+        generoModel = generoRepository.save(generoModel);
+
+        grupoModel = GrupoModel.builder().id(1).nomeGrupo("ADMIN").build();
+        grupoModel = grupoRepository.save(grupoModel);
+        Set<GrupoModel> grupos = new HashSet<>();
+        grupos.add(grupoModel);
+
+        usuarioModel = UsuarioModel.builder()
+                .id(1)
+                .nome("John")
+                .username("john123")
+                .email("john@example.com")
+                .password("password123")
+                .grupos(grupos)
+                .build();
+        usuarioModel = usuarioRepository.save(usuarioModel);
+
+        egressoModel = EgressoModel.builder()
+                .id(1)
+                .nascimento(LocalDate.parse("1999-10-20"))
+                .genero(generoModel)
+                .matricula("202003940011")
+                .interesseEmPos(true)
+                .lattes("null")
+                .linkedin("null")
+                .usuario(usuarioModel)
+                .build();
+        egressoModel.setCreatedBy(usuarioModel);
+
+        egressoModel = egressoRepository.save(egressoModel);
+
+        setorAtuacaoModel = SetorAtuacaoModel.builder()
+            .id(ID)
+            .nome(NOME)
+            .build();
+
+        setorAtuacaoModel.setCreatedBy(usuarioModel);
+
+        setorAtuacaoModel = setorAtuacaoRepository.save(setorAtuacaoModel);
+    }
+
+    /**
+     * Método que testa o repositório que retorna todos os SetorAtuacaos
+     *
+     * @author Pedro Inácio
+     * @since 10/06/2023
+     */
+    @Test
+    void testFindAll() {
+        List<SetorAtuacaoModel> response = setorAtuacaoRepository.findAll();
+
+        assertNotNull(response);
+        assertEquals(List.of(setorAtuacaoModel), response);
+    }
+    
+    @Test
+    void testFindByNomeIgnoreCase() {
+        Optional<SetorAtuacaoModel> response = setorAtuacaoRepository.findByNomeIgnoreCase(setorAtuacaoModel.getNome());
+        
+        assertNotNull(response);
+        assertEquals(setorAtuacaoModel, response.get());
+    }
+    
+    @Test
+    void testExistsByNomeIgnoreCase() {
+        Boolean response = setorAtuacaoRepository.existsByNomeIgnoreCase(setorAtuacaoModel.getNome());
+
+        assertNotNull(response);
+        assertTrue(response);
+    }
+
+    /**
+     * Metodo para remover todos os dados do repository.
+     *
+     * @author Pedro Inácio
+     * @since 10/06/2023
+     */
+    @AfterAll
+    public void tearDown() {
+        setorAtuacaoRepository.deleteAll();
+    }
+}
