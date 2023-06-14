4a2339ef83ee9345fb46f46f3a6e7799a3c86ed0
hawkilol <abacates21@gmail.com>
2023-06-14 14:42:28 -0300

    fix(dialog): posição absoluta do dialog

--- frontend/src/components/CustomDialog.vue
+++ frontend/src/components/CustomDialog.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="modelValue">
-    <div class="inset-0 fixed top-0 left-0 bg-black/75 flex items-center justify-center">
+    <div class="z-10 inset-0 fixed top-0 left-0 bg-black/75 flex items-center justify-center">
       <div
-        class="bg-white rounded-xl w-80 h-64 sm:w-10/12 sm:h-96 max-w-2xl relative overflow-hidden"
+        class="z-10 bg-white rounded-xl w-80 h-64 sm:w-10/12 sm:h-96 max-w-2xl relative overflow-hidden"
         :class="{
           ['scale-0']: !modelValue
         }"
