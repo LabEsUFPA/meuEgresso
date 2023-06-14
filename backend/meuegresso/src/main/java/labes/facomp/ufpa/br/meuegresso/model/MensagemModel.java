6c4017859b1c26bdc52904298f99fa67d7341678
chsantos234 <camilosantos1709@gmail.com>
2023-06-14 02:43:46 -0300

    feat: add models e funcoes de mensagem

---
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/model/MensagemModel.java
@@ -0,0 +1,30 @@
+package labes.facomp.ufpa.br.meuegresso.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "mensagem")
+public class MensagemModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_mensagem", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "mensagem_mensagem", unique = false, nullable = false)
+    private String mensagem;
+
+    @Column(name = "escopo_mensagem", unique = false, nullable = false)
+    private String escopo;
+
+}
--- backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/repository/usuario/UsuarioRepository.java
+++ backend/meuegresso/src/main/java/labes/facomp/ufpa/br/meuegresso/repository/usuario/UsuarioRepository.java
@@ -3,6 +3,7 @@ package labes.facomp.ufpa.br.meuegresso.repository.usuario;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import labes.facomp.ufpa.br.meuegresso.model.UsuarioModel;
@@ -27,4 +28,12 @@ public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer>
 	boolean existsByIdAndCreatedById(Integer id, Integer createdBy);
 
 	boolean existsByUsernameIgnoreCase(String username);
+
+	@Query(value = """
+			SELECT u.nome_usuario, u.email
+			FROM usuario u
+			JOIN egresso e ON u.id_usuario = e.usuario_id
+			WHERE e.created_date < NOW() - INTERVAL '1 year';
+			""")
+	List<String> sendEmail();
 }
