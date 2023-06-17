2b6184dfb847b4c57673d8f447878ccd1a8d7037
Gustavo Rodrigues <gustavo.hwmanz@gmail.com>
2023-06-17 20:00:54 -0300

    feat: guardas de navegação

--- frontend/src/components/CustomDialog.vue
+++ frontend/src/components/CustomDialog.vue
@@ -2,7 +2,7 @@
   <div v-if="modelValue">
     <div class="inset-0 fixed top-0 left-0 bg-black/75 flex items-center justify-center">
       <div
-        class="bg-white rounded-xl w-80 h-64 sm:w-10/12 sm:h-96 max-w-2xl relative"
+        class="bg-white rounded-xl w-80 h-fit min-h-[16rem] sm:w-10/12 sm:h-96 max-w-2xl relative"
         :class="{
           ['scale-0']: !modelValue
         }"
@@ -23,10 +23,7 @@
             Ir para o cadastro
           </CustomButton>
         </div>
-        <div
-          v-else
-          class="h-full"
-        >
+        <div class="h-full">
           <CustomButton
             class="absolute right-0"
             color="blue"
