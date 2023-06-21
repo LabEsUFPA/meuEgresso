d4dc946a83af56a5fef5a5c63865b8361f188e31
chsantos234 <camilosantos1709@gmail.com>
2023-06-21 11:48:28 -0300

    fix: correcao inicial EgressoEmpresaCont test

--- backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/controller/egresso/EgressoEmpresaControllerTest.java
+++ backend/meuegresso/src/test/java/labes/facomp/ufpa/br/meuegresso/controller/egresso/EgressoEmpresaControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -59,6 +60,8 @@ import labes.facomp.ufpa.br.meuegresso.repository.empresa.EmpresaRepository;
 import labes.facomp.ufpa.br.meuegresso.repository.faixasalarial.FaixaSalarialRepository;
 import labes.facomp.ufpa.br.meuegresso.repository.genero.GeneroRepository;
 import labes.facomp.ufpa.br.meuegresso.repository.setoratuacao.SetorAtuacaoRepository;
+import labes.facomp.ufpa.br.meuegresso.repository.usuario.UsuarioRepository;
+
 //TODO consertar teste
 @SpringBootTest
 @DirtiesContext
@@ -68,237 +71,246 @@ import labes.facomp.ufpa.br.meuegresso.repository.setoratuacao.SetorAtuacaoRepos
 @TestMethodOrder(OrderAnnotation.class)
 class EgressoEmpresaControllerTest {
 
-    static final Integer EMPRESA_ID = 1;
-    static final String NOME = "EmpresaTeste";
-    static final String SETORATUACAO = "SetorTeste";
+	static final Integer EMPRESA_ID = 1;
+	static final String NOME = "EmpresaTeste";
+	static final String SETORATUACAO = "SetorTeste";
 
-    final String USERNAME = "username_test";
+	final String USERNAME = "username_test";
 
-    static final Integer EGRESSO_ID = 1;
-    static final LocalDate EGRESSO_NASCIMENTO = LocalDate.parse("1999-10-20");
-    static final String EGRESSO_EMAIL = "cantao162@gmail.com";
+	static final Integer EGRESSO_ID = 1;
+	static final LocalDate EGRESSO_NASCIMENTO = LocalDate.parse("1999-10-20");
+	static final String EGRESSO_EMAIL = "cantao162@gmail.com";
 
-    static final Integer FAIXASALARIAL_ID = 1;
-    static final String FAIXASALARIAL = "5000 - 15000";
+	static final Integer FAIXASALARIAL_ID = 1;
+	static final String FAIXASALARIAL = "5000 - 15000";
 
-    @Autowired
-    private EgressoRepository egressoRepository;
+	@Autowired
+	private EgressoRepository egressoRepository;
 
-    @Autowired
-    private GeneroRepository generoRepository;
+	@Autowired
+	UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private EmpresaRepository empresaRepository;
+	@Autowired
+	private GeneroRepository generoRepository;
 
-    @Autowired
-    private FaixaSalarialRepository faixaSalarialRepository;
+	@Autowired
+	private EmpresaRepository empresaRepository;
 
-    @Autowired
-    private AreaAtuacaoRepository areaAtuacaoRepository;
+	@Autowired
+	private FaixaSalarialRepository faixaSalarialRepository;
 
-    @Autowired
-    private SetorAtuacaoRepository setorAtuacaoRepository;
+	@Autowired
+	private AreaAtuacaoRepository areaAtuacaoRepository;
 
-    @Autowired
-    MockMvc mockMvc;
+	@Autowired
+	private SetorAtuacaoRepository setorAtuacaoRepository;
 
-    String token;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
-    UsuarioModel usuarioModel;
+	@Autowired
+	MockMvc mockMvc;
 
-    EmpresaBasicDTO empresaBasicDTO;
-    EmpresaModel empresaModel;
+	String token;
 
-    EgressoEmpresaDTO egressoEmpresaDTO;
-    EgressoEmpresaModelId egressoEmpresaModelId;
+	UsuarioModel usuarioModel;
 
-    EgressoPublicDTO egressoPublicDTO;
-    EgressoCadastroDTO egressoCadastroDTO;
-    EgressoModel egressoModel;
+	EmpresaBasicDTO empresaBasicDTO;
+	EmpresaModel empresaModel;
 
-    FaixaSalarialDTO faixaSalarialDTO;
+	EgressoEmpresaDTO egressoEmpresaDTO;
+	EgressoEmpresaModelId egressoEmpresaModelId;
 
-    @Autowired
-    ModelMapper modelMapper;
+	EgressoPublicDTO egressoPublicDTO;
+	EgressoCadastroDTO egressoCadastroDTO;
+	EgressoModel egressoModel;
 
-    @BeforeAll
-    void setUp() throws Exception {
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+	FaixaSalarialDTO faixaSalarialDTO;
 
-        /* Gênero */
-        GeneroModel genero = new GeneroModel(1, "genero X");
-        GeneroDTO generoDTO = modelMapper.map(genero, GeneroDTO.class);
-        genero = generoRepository.save(genero);
+	@Autowired
+	ModelMapper modelMapper;
 
-        /* Egresso */
-        egressoPublicDTO = EgressoPublicDTO.builder().id(EGRESSO_ID).email(EGRESSO_EMAIL)
-                .genero(generoDTO).build();
-        egressoModel = modelMapper.map(egressoPublicDTO, EgressoModel.class);
-        egressoRepository.save(this.egressoModel);
+	@BeforeAll
+	void setUp() throws Exception {
+		ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
 
-        /* Empresa */
-        empresaBasicDTO = EmpresaBasicDTO.builder().id(EMPRESA_ID).nome(NOME).build();
-        empresaModel = modelMapper.map(empresaBasicDTO, EmpresaModel.class);
-        empresaRepository.save(empresaModel);
+		usuarioModel = new UsuarioModel();
+		usuarioModel.setUsername(USERNAME);
+		usuarioModel.setNome("nome_test");
+		usuarioModel.setEmail("teste@gmail.com");
+		usuarioModel.setGrupos(Set.of(Grupos.ADMIN));
 
-        /* ModelId */
-        egressoEmpresaModelId = EgressoEmpresaModelId.builder().egressoId(EGRESSO_ID).empresaId(EMPRESA_ID).build();
+		final String plainTextPassword = "teste123";
+		final String encodedPassword = passwordEncoder.encode(plainTextPassword);
 
-        /* FaixaSalarial */
-        faixaSalarialDTO = FaixaSalarialDTO.builder().id(FAIXASALARIAL_ID).faixa(FAIXASALARIAL).build();
-        FaixaSalarialModel faixaSalarialModel = modelMapper.map(faixaSalarialDTO, FaixaSalarialModel.class);
-        faixaSalarialRepository.save(faixaSalarialModel);
+		usuarioModel.setPassword(encodedPassword);
+		usuarioRepository.save(usuarioModel);
 
-        /* AreaAtuacao */
-        AreaAtuacaoDTO areaAtuacaoDTO= AreaAtuacaoDTO.builder().id(1).nome("areaTeste").build();
-        AreaAtuacaoModel areaAtuacaoModel = modelMapper.map(areaAtuacaoDTO, AreaAtuacaoModel.class);
-        areaAtuacaoRepository.save(areaAtuacaoModel);
+		AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+		authenticationRequest.setUsername(usuarioModel.getUsername());
+		authenticationRequest.setPassword(plainTextPassword);
+		String objectJson = objectMapper.writeValueAsString(authenticationRequest);
 
-        /* SetorAtuacao */
-        SetorAtuacaoDTO setorAtuacaoDTO= SetorAtuacaoDTO.builder().id(1).nome("setorTeste").build();
-        SetorAtuacaoModel setorAtuacaoModel = modelMapper.map(setorAtuacaoDTO, SetorAtuacaoModel.class);
-        setorAtuacaoRepository.save(setorAtuacaoModel);
+		/* Gênero */
+		GeneroModel genero = new GeneroModel(1, "genero X");
+		GeneroDTO generoDTO = modelMapper.map(genero, GeneroDTO.class);
+		genero = generoRepository.save(genero);
 
-        /* EgressoEmpresa */
-        egressoEmpresaDTO = EgressoEmpresaDTO.builder().id(egressoEmpresaModelId).egresso(egressoPublicDTO)
-                .empresa(empresaBasicDTO).areaAtuacao("AreaTesteEE").faixaSalarial(faixaSalarialDTO).build();
+		/* Empresa */
+		empresaBasicDTO = EmpresaBasicDTO.builder().id(EMPRESA_ID).nome(NOME).build();
+		empresaModel = modelMapper.map(empresaBasicDTO, EmpresaModel.class);
+		empresaRepository.save(empresaModel);
 
-        usuarioModel = new UsuarioModel();
-        usuarioModel.setUsername(USERNAME);
-        usuarioModel.setNome("nome_test");
-        usuarioModel.setEmail("teste@gmail.com");
-        usuarioModel.setPassword("teste123");
-        usuarioModel.setGrupos(Set.of(Grupos.ADMIN));
+		/* ModelId */
+		egressoEmpresaModelId = EgressoEmpresaModelId.builder().egressoId(EGRESSO_ID).empresaId(EMPRESA_ID).build();
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/auth/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(usuarioModel)))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isCreated())
-                .andReturn();
+		/* FaixaSalarial */
+		faixaSalarialDTO = FaixaSalarialDTO.builder().id(FAIXASALARIAL_ID).faixa(FAIXASALARIAL).build();
+		FaixaSalarialModel faixaSalarialModel = modelMapper.map(faixaSalarialDTO, FaixaSalarialModel.class);
+		faixaSalarialRepository.save(faixaSalarialModel);
 
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
-        authenticationRequest.setUsername(usuarioModel.getUsername());
-        authenticationRequest.setPassword(usuarioModel.getPassword());
-        String objectJson = objectMapper.writeValueAsString(authenticationRequest);
+		/* AreaAtuacao */
+		AreaAtuacaoDTO areaAtuacaoDTO = AreaAtuacaoDTO.builder().id(1).nome("areaTeste").build();
+		AreaAtuacaoModel areaAtuacaoModel = modelMapper.map(areaAtuacaoDTO, AreaAtuacaoModel.class);
+		areaAtuacaoRepository.save(areaAtuacaoModel);
 
-        MvcResult resultado = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectJson))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andReturn();
+		/* SetorAtuacao */
+		SetorAtuacaoDTO setorAtuacaoDTO = SetorAtuacaoDTO.builder().id(1).nome("setorTeste").build();
+		SetorAtuacaoModel setorAtuacaoModel = modelMapper.map(setorAtuacaoDTO, SetorAtuacaoModel.class);
+		setorAtuacaoRepository.save(setorAtuacaoModel);
 
-        AuthenticationResponse authenticationResponse = objectMapper.readValue(
-                resultado.getResponse().getContentAsString(), AuthenticationResponse.class);
-        this.token = authenticationResponse.getToken();
+		/* Egresso */
+		egressoPublicDTO = EgressoPublicDTO.builder().id(EGRESSO_ID).email(EGRESSO_EMAIL).genero(generoDTO).build();
+		// egressoModel = modelMapper.map(egressoPublicDTO, EgressoModel.class);
+		EgressoModel egressoModel = EgressoModel.builder()
+				.id(EGRESSO_ID)
+				.nascimento(EGRESSO_NASCIMENTO)
+				.genero(genero)
+				.matricula("303005550011")
+				.usuario(usuarioModel)
+				.build();
+		egressoRepository.save(egressoModel);
 
-        MvcResult resposta = mockMvc.perform(
-                MockMvcRequestBuilders.get("/usuario")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + this.token))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
+		/* EgressoEmpresa */
+		egressoEmpresaDTO = EgressoEmpresaDTO.builder().id(egressoEmpresaModelId).egresso(egressoPublicDTO)
+				.empresa(empresaBasicDTO).areaAtuacao("AreaTesteEE").faixaSalarial(faixaSalarialDTO).build();
 
-        UsuarioAuthDTO usuarioAuthDTO = objectMapper.readValue(resposta.getResponse().getContentAsString(),
-                UsuarioAuthDTO.class);
+		MvcResult resultado = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectJson))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andReturn();
 
-        usuarioModel.setId(usuarioAuthDTO.getId());
+		AuthenticationResponse authenticationResponse = objectMapper.readValue(
+			resultado.getResponse().getContentAsString(), AuthenticationResponse.class);
+		this.token = authenticationResponse.getToken();
 
-    }
+		MvcResult resposta = mockMvc.perform(
+			MockMvcRequestBuilders.get("/usuario")
+					.contentType(MediaType.APPLICATION_JSON)
+					.header("Authorization", "Bearer " + this.token))
+			.andDo(MockMvcResultHandlers.print())
+			.andExpect(status().isOk()).andReturn();
 
-    @Test
-    @Order(1)
-    void testCadastrarEgressoEmpresa() throws Exception {
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+		UsuarioAuthDTO usuarioAuthDTO = objectMapper.readValue(resposta.getResponse().getContentAsString(),
+				UsuarioAuthDTO.class);
 
-        MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.post("/egressoEmpresa")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + this.token)
-                .content(objectMapper.writeValueAsString(egressoEmpresaDTO)))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isCreated())
-                .andReturn();
+		usuarioModel.setId(usuarioAuthDTO.getId());
 
-        String retornoString = resposta.getResponse().getContentAsString();
-        assertEquals(ResponseType.SUCCESS_SAVE.getMessage(), retornoString);
-    }
+	}
 
-    @Test
-    @Order(2)
-    void testFindById() throws Exception {
-        //TODO: fazer setorAtuacaoController e areaAtuacaoController pra funcionar
+	@Test
+	@Order(1)
+	void testCadastrarEgressoEmpresa() throws Exception {
+		ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
 
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+		MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.post("/egressoEmpresa")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + this.token)
+				.content(objectMapper.writeValueAsString(egressoEmpresaDTO)))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isCreated())
+				.andReturn();
 
-        MvcResult resposta = mockMvc
-                .perform(MockMvcRequestBuilders
-                        .get("/egressoEmpresa?egressoId=" + egressoPublicDTO.getId() + "&empresaId="
-                                + empresaBasicDTO.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + this.token))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
+		String retornoString = resposta.getResponse().getContentAsString();
+		assertEquals(ResponseType.SUCCESS_SAVE.getMessage(), retornoString);
+	}
 
-        EgressoEmpresaDTO egressoEmpresaDTOresponse = objectMapper
-                .readValue(resposta.getResponse().getContentAsString(), EgressoEmpresaDTO.class);
-        assertEquals(NOME, egressoEmpresaDTOresponse.getEmpresa().getNome());
+	@Test
+	@Order(2)
+	void testConsultarEgressoEmpresas() throws Exception {
 
-    }
+		ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
 
-    @Test
-    @Order(3)
-    void testAtualizarEgressoEmpresa() throws Exception {
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+		MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.get("/egressoEmpresa")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + this.token))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk()).andReturn();
 
-        MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.put("/egressoEmpresa")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + this.token)
-                .content(objectMapper.writeValueAsString(egressoEmpresaDTO)))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isCreated())
-                .andReturn();
+		List<EgressoEmpresaDTO> egressoEmpresasDTO = objectMapper.readValue(resposta.getResponse().getContentAsString(),
+				new TypeReference<List<EgressoEmpresaDTO>>() {});
 
-        String retornoString = resposta.getResponse().getContentAsString();
-        assertEquals(ResponseType.SUCCESS_UPDATE.getMessage(), retornoString);
+		assertNotNull(egressoEmpresasDTO);
+		assertEquals(1, egressoEmpresasDTO.size());
 
-    }
+	}
 
-    @Test
-    @Order(4)
-    void testConsultarEgressoEmpresas() throws Exception {
+	@Test
+	@Order(3)
+	void testFindById() throws Exception {
+		// TODO: fazer setorAtuacaoController e areaAtuacaoController pra funcionar
 
-        ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+		ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
 
-        MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.get("/egressoEmpresa")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + this.token))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk()).andReturn();
+		MvcResult resposta = mockMvc
+				.perform(MockMvcRequestBuilders
+						.get("/egressoEmpresa?egressoId=" + egressoPublicDTO.getId() + "&empresaId="
+								+ empresaBasicDTO.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.header("Authorization", "Bearer " + this.token))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk()).andReturn();
 
-        List<EgressoEmpresaDTO> egressoEmpresasDTO = objectMapper.readValue(resposta.getResponse().getContentAsString(),
-                new TypeReference<List<EgressoEmpresaDTO>>() {
-                });
+		EgressoEmpresaDTO egressoEmpresaDTOresponse = objectMapper
+				.readValue(resposta.getResponse().getContentAsString(), EgressoEmpresaDTO.class);
+		assertEquals(NOME, egressoEmpresaDTOresponse.getEmpresa().getNome());
 
-        assertNotNull(egressoEmpresasDTO);
-        assertEquals(1, egressoEmpresasDTO.size());
+	}
 
-    }
+	@Test
+	@Order(4)
+	void testAtualizarEgressoEmpresa() throws Exception {
+		ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
 
-    @Test
-    @Order(5)
-    void testDeleteById() throws Exception {
-        MvcResult resposta = mockMvc.perform(
-                MockMvcRequestBuilders
-                        .delete("/egressoEmpresa?egressoId=" + egressoPublicDTO.getId() + "&empresaId="
-                                + empresaBasicDTO.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + this.token))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andReturn();
-        String resultado = resposta.getResponse().getContentAsString();
-        assertEquals(ResponseType.SUCCESS_DELETE.getMessage(), resultado);
-    }
+		MvcResult resposta = mockMvc.perform(MockMvcRequestBuilders.put("/egressoEmpresa")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Bearer " + this.token)
+				.content(objectMapper.writeValueAsString(egressoEmpresaDTO)))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isCreated())
+				.andReturn();
+
+		String retornoString = resposta.getResponse().getContentAsString();
+		assertEquals(ResponseType.SUCCESS_UPDATE.getMessage(), retornoString);
+
+	}
+
+	@Test
+	@Order(5)
+	void testDeleteById() throws Exception {
+		MvcResult resposta = mockMvc.perform(
+				MockMvcRequestBuilders
+						.delete("/egressoEmpresa?egressoId=" + egressoPublicDTO.getId() + "&empresaId="
+								+ empresaBasicDTO.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.header("Authorization", "Bearer " + this.token))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(status().isOk())
+				.andReturn();
+		String resultado = resposta.getResponse().getContentAsString();
+		assertEquals(ResponseType.SUCCESS_DELETE.getMessage(), resultado);
+	}
 
 }
