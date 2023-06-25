d2769b30129d9e034cb051252352d09fc9ef5408
Marcus Loureiro <marcushuriel08@hotmail.com>
2023-06-24 21:46:39 -0300

    refactor: rotas filhas

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -7,97 +7,103 @@ export default [
       { path: '/entrar', component: async () => await import('src/pages/PaginaLogin/PaginaLogin.vue') },
       {
         path: '/cadastro',
-        component: async () => await import('src/pages/CadastroEgresso/CadastroEgresso.vue'),
-        meta: {
-          shouldNotForce: true,
-          requiresAuthAdmin: true,
-          allowedScopes: ['EGRESSO']
-        }
+        component: async () => await import ('src/components/HeaderAndFooter.vue'),
+        children: [
+          { path: '/', component: async () => await import ('src/pages/CadastroEgresso/CadastroEgresso.vue') }, // Redirecionar para /cadastro/egresso por padrão
+          {
+            path: '/egresso',
+            component: async () => await import ('src/pages/CadastroEgresso/CadastroEgresso.vue'),
+            meta: {
+              shouldNotForce: true,
+              requiresAuthAdmin: true,
+              allowedScopes: ['EGRESSO']
+            }
+          },
+          { path: '/perfil', component: async () => await import ('src/pages/CadastroPerfil/CadastroPerfil.vue') },
+          {
+            path: '/admin',
+            component: async () => await import ('src/pages/CadastroAdmin/CadastroAdmin.vue'),
+            meta: {
+              requiresAuthAdmin: true,
+              allowedScopes: ['ADMIN', 'SECRETARIO']
+            }
+          },
+          {
+            path: '/anuncio',
+            component: async () => await import ('src/pages/CadastroAnuncio/CadastroAnuncio.vue'),
+            meta: {
+              requiresAuth: true
+            }
+          }
+        ]
       },
       {
-        path: '/cadastro-perfil',
-        component: async () => await import('src/pages/CadastroPerfil/CadastroPerfil.vue')
-      },
-      {
-        path: '/cadastro-admin',
-        component: async () => await import('src/pages/CadastroAdmin/CadastroAdmin.vue'),
+        path: '/admin',
         meta: {
           requiresAuthAdmin: true,
           allowedScopes: ['ADMIN', 'SECRETARIO']
-        }
+        },
+        children: [
+          { path: '/', component: async () => await import ('src/pages/PainelAdmin/PainelAdmin.vue') },
+          {
+            path: '/painel',
+            component: async () => await import ('src/pages/PainelAdmin/PainelAdmin.vue'),
+            children: [
+              {
+                path: '/email',
+                component: async () => await import ('src/pages/EmailAdmin/EmailAdmin.vue')
+              },
+              {
+                path: '/registro',
+                component: async () => await import ('src/pages/RegistroEgressos/RegistroEgressos.vue')
+              },
+              {
+                path: '/notificacoes',
+                component: async () => await import ('src/pages/NotificacoesAdmin/NotificacoesAdmin.vue')
+              }
+            ]
+          }
+
+        ]
       },
       {
-        path: '/cadastro-anuncio',
-        component: async () => await import('src/pages/CadastroAnuncio/CadastroAnuncio.vue'),
+        path: '/egresso/:id?',
+        component: async () => await import ('src/pages/PerfilEgresso/PerfilEgresso.vue')
+      },
+      {
+        path: '/conta-egresso',
+        component: async () => await import ('src/pages/EdicaoContaEgresso/EdicaoContaEgresso.vue'),
         meta: {
           requiresAuth: true
         }
       },
       {
-        path: '/notificacoes-admin',
-        component: async () => await import('src/pages/NotificacoesAdmin/NotificacoesAdmin.vue'),
-        meta: {
-          requiresAuthAdmin: true,
-          allowedScopes: ['ADMIN', 'SECRETARIO']
-        }
-      },
-      {
-        path: '/egresso',
-        component: async () => await import('src/pages/PerfilEgresso/PerfilEgresso.vue')
-      },
-      {
-        path: '/egresso/:id',
-        component: async () => await import('src/pages/PerfilEgresso/PerfilEgresso.vue')
-      },
-      {
-        path: '/conta-egresso',
-        component: async () => await import('src/pages/EdicaoContaEgresso/EdicaoContaEgresso.vue')
-      },
-      {
         path: '/conta-admin',
-        component: async () => await import('src/pages/EdicaoContaAdmin/EdicaoContaAdmin.vue')
+        component: async () => await import ('src/pages/EdicaoContaAdmin/EdicaoContaAdmin.vue'),
+        meta: {
+          requiresAuth: true
+        }
       },
       {
         path: '/graficos',
-        component: async () => await import('src/pages/PaginaGraficos/PaginaGraficos.vue')
+        component: async () => await import ('src/pages/PaginaGraficos/PaginaGraficos.vue')
       },
       {
         path: '/vagas',
-        component: async () => await import('src/pages/FeedVagas/FeedVagas.vue')
+        component: async () => await import ('src/pages/FeedVagas/FeedVagas.vue'),
+        children: [
+          {
+            path: '/:id',
+            component: async () => await import ('src/pages/AnuncioVaga/AnuncioVaga.vue')
+          }
+        ]
       },
+
       {
-        path: '/email',
-        component: async () => await import('src/pages/EmailAdmin/EmailAdmin.vue'),
-        meta: {
-          requiresAuthAdmin: true,
-          allowedScopes: ['ADMIN', 'SECRETARIO']
-        }
-      },
-      {
-        path: '/vagas/:id',
-        component: async () => await import('src/pages/AnuncioVaga/AnuncioVaga.vue')
-      },
-      {
-        path: '/painel-admin',
-        component: async () => await import('src/pages/PainelAdmin/PainelAdmin.vue'),
-        meta: {
-          requiresAuthAdmin: true,
-          allowedScopes: ['ADMIN', 'SECRETARIO']
-        }
-      },
-      {
-        path: '/registro-egressos',
-        component: async () => await import('src/pages/RegistroEgressos/RegistroEgressos.vue'),
-        meta: {
-          requiresAuthAdmin: true,
-          allowedScopes: ['ADMIN', 'SECRETARIO']
-        }
-      },
-      {
-        path: 'redefinir-senha',
-        component: async () => await import('src/pages/RedefinirSenha/RedefinirSenha.vue')
+        path: '/redefinir-senha',
+        component: async () => await import ('src/pages/RedefinirSenha/RedefinirSenha.vue')
       }
     ]
   },
-  { path: '/:pathMatch(.*)*', component: async () => await import('src/pages/PageNotFound/PageNotFound.vue') }
+  { path: '/:pathMatch(.*)*', component: async () => await import ('src/pages/PageNotFound/PageNotFound.vue') }
 ]
