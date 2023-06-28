513fe7cc5242db6194a37db2901572ef21cae47c
jonas-al <yonas.alluniverse@gmail.com>
2023-06-28 11:33:44 -0300

    feat: css mdi para o componente CustomDatepicker

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import money from 'v-money3'
 import '@oruga-ui/oruga-next/dist/oruga.css'
 import '@oruga-ui/oruga-next/dist/oruga-full.css'
+import '@mdi/font/css/materialdesignicons.min.css'
 import './style.scss'
 import VueScrollTo from 'vue-scrollto'
 const pinia = createPinia()
