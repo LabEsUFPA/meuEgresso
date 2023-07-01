c72d91f5a45007869c551f911c1119c62495310d
Gustavo Rodrigues <gustavo.hwmanz@gmail.com>
2023-06-30 23:47:21 -0300

    fix: remoção de valores do form para egresso desempregado

--- frontend/src/pages/CadastroEgresso/CadastroEgresso.vue
+++ frontend/src/pages/CadastroEgresso/CadastroEgresso.vue
@@ -276,6 +276,7 @@
               name="carreira.setor"
               label="Setor de Atuação"
               placeholder="Selecione"
+              ref="selectSetor"
               :options="selectOpts.setorAtuacao"
               :required="area !== 'Desempregado'"
               :disabled="area === 'Desempregado'"
@@ -285,6 +286,7 @@
               class="mb-5"
               name="carreira.empresa"
               label="Empresa"
+              ref="inputEmpresa"
               placeholder="Ex: Google"
               :required="area !== 'Desempregado'"
               :disabled="area === 'Desempregado'"
@@ -295,6 +297,7 @@
               class="mb-5"
               name="carreira.faixaSalarial"
               label="Faixa Salarial"
+              ref="selectFaixa"
               :options="$storeCadastro.faixasSalariais"
               :required="area !== 'Desempregado'"
               :disabled="area === 'Desempregado'"
@@ -309,27 +312,33 @@
             class="mb-5"
             name="carreira.pais"
             label="País"
+            ref="selectPais"
             :options="countries"
             v-model:value="pais"
             @change="pais = $event"
             :disabled="area === 'Desempregado'"
+            :required="area !== 'Desempregado'"
           />
 
           <CustomSelect
             class="mb-5"
             name="carreira.estado"
             label="Estado"
+            ref="selectEstado"
             :options="states"
             v-model:value="estado"
             @change="estado = $event"
             :disabled="area === 'Desempregado'"
+            :required="area !== 'Desempregado'"
           />
 
           <CustomSelect
             name="carreira.cidade"
             label="Cidade"
+            ref="selectCidade"
             :options="cities"
             :disabled="area === 'Desempregado'"
+            :required="area !== 'Desempregado'"
           />
         </template>
       </FolderSection>
@@ -569,9 +578,16 @@ const pais = ref('')
 const estado = ref('')
 const area = ref('')
 const temFoto = ref(false)
-const form = ref<typeof Form | null>(null)
+const form = ref()
 const errorText = ref('')
 const error = ref(false)
+
+const selectSetor = ref()
+const selectFaixa = ref()
+const inputEmpresa = ref()
+const selectPais = ref()
+const selectEstado = ref()
+const selectCidade = ref()
 
 const minDate = ref(new Date(-8640000000000000))
 const eighteenYearsAgo = ref(new Date())
@@ -859,43 +875,48 @@ const schema = object().shape({
 })
 
 onMounted(async () => {
-  const estadoInput = document.querySelector('.localizacao-estado') as HTMLInputElement
-  const cidadeInput = document.querySelector('.localizacao-cidade') as HTMLInputElement
   watch(pais, () => {
-    form.value?.setFieldValue('localizacao.cidade', '')
-    form.value?.setFieldValue('localizacao.estado', '')
-    setTimeout(() => {
-      estadoInput.value = ''
-      cidadeInput.value = ''
-    }, 10)
+    selectEstado.value.setInitialValues('')
+    form.value.setFieldTouched('carreira.estado', false)
   })
 
   watch(estado, () => {
-    form.value?.setFieldValue('localizacao.cidade', '')
-    setTimeout(() => {
-      cidadeInput.value = ''
-    }, 10)
+    selectCidade.value.setInitialValues('')
+    form.value.setFieldTouched('carreira.cidade', false)
   })
 
   watch(compCotista, (_, oldVal) => {
     if (oldVal) {
       ['renda', 'escola', 'raca', 'quilombolaIndigena', 'pcd'].forEach(field => {
-        form.value?.setFieldValue(`academico.cotista.tipos.${field}`, false)
+        form.value.setFieldValue(`academico.cotista.tipos.${field}`, false)
       })
+    }
+  })
+
+  watch(area, (newVal) => {
+    if (newVal === 'Desempregado') {
+      selectSetor.value.setInitialValues('')
+      form.value.setFieldTouched('carreira.setor', false)
+      selectFaixa.value.setInitialValues('')
+      form.value.setFieldTouched('carreira.faixaSalarial', false)
+      form.value.setFieldValue('carreira.empresa', '')
+      form.value.setFieldTouched('carreira.empresa', false)
+      selectPais.value.setInitialValues('')
+      form.value.setFieldTouched('carreira.pais', false)
     }
   })
 
   if (storage.has('loggedUser') && $route.params.id === undefined) {
     const userData = JSON.parse(storage.get('loggedUser'))
 
-    form.value?.setFieldValue('geral.email', userData.email)
-    form.value?.setFieldValue('geral.nome', userData.nomeCompleto)
+    form.value.setFieldValue('geral.email', userData.email)
+    form.value.setFieldValue('geral.nome', userData.nomeCompleto)
   } else if ($route.params.id !== undefined) {
     const userData = await $storeCadastro.fetchUserData(Number($route.params.id))
 
     if (typeof userData !== 'number') {
-      form.value?.setFieldValue('geral.email', userData.email)
-      form.value?.setFieldValue('geral.nome', userData.nome)
+      form.value.setFieldValue('geral.email', userData.email)
+      form.value.setFieldValue('geral.nome', userData.nome)
     }
   }
 })
