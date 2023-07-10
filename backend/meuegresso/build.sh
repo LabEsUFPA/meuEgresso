d2e46870371b5d71fd246f4f0c36c5f8599ba7f5
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-07-10 17:33:16 -0300

    fix: ci

--- backend/meuegresso/build.sh
+++ backend/meuegresso/build.sh
@@ -2,7 +2,7 @@
 
 [ ! -d "./meuEgressoFotos" ] && echo "Diretorio 'meuEgressoFotos' não existe, logo não é possivel armazenar as fotos dos egressos." && exit 1;
 
-[ ! -d "./.env" ] && echo "Arquivo de configuração '.env' não existe, consulte o manual" && exit 1;
+[ ! -f "./.env" ] && echo "Arquivo de configuração '.env' não existe, consulte o manual" && exit 1;
 
 cat << EOF > run.sh
 #!/bin/bash
--- backend/meuegresso/deploy_traefik.sh
+++ backend/meuegresso/deploy_traefik.sh
@@ -2,6 +2,6 @@
 
 [ ! -d "./meuEgressoFotos" ] && echo "Diretorio 'meuEgressoFotos' não existe, logo não é possivel armazenar as fotos dos egressos." && exit 1;
 
-[ ! -d "./.env" ] && echo "Arquivo de configuração '.env' não existe, consulte o manual" && exit 1;
+[ ! -f "./.env" ] && echo "Arquivo de configuração '.env' não existe, consulte o manual" && exit 1;
 
 [ -d "./meuEgressoFotos" ] && docker pull alverad/meu-egresso-api:latest && docker stack deploy -c <(docker-compose -f docker-compose.api.yml config) meuegresso-api
